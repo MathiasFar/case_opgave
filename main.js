415040e1f16a80865a89f50985ccb47b4d26489d
MathiasFar <mathiassvarre06@gmail.com>
2022-09-21 08:54:09 +0200

    boilerplate

--- main.js
+++
@@ -1,6 +0,0 @@
-const parallax = document.getElementsById("imgDiv1");
-
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 1 + "px";
-})
